e85e259d0840f96bb0feb1eed4473ce01191a3db
Matthieu Falce <matthieu@falce.net>
2022-12-07 16:11:42 +0100

    gitautosync update 167

--- tape_en_cours/mysite/polls/admin.py
+++ tape_en_cours/mysite/polls/admin.py
@@ -37,8 +37,8 @@ class QuestionAdmin(admin.ModelAdmin):
 class ChoiceAdmin(admin.ModelAdmin):
     def get_field_queryset(self, db, db_field, request):
         queryset = super().get_field_queryset(db, db_field, request)
-        # if db_field.name == "question":
-        #     queryset = queryset.order_by("question_text")
+        if queryset and db_field.name == "question":
+            queryset = queryset.order_by("question_text")
         print(queryset)
         return queryset
 
