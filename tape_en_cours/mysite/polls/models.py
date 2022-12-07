c3081f05cd37979047a65a71ae7b4fda31e7531a
Matthieu Falce <matthieu@falce.net>
2022-12-07 14:12:06 +0100

    gitautosync update 102

--- tape_en_cours/mysite/polls/models.py
+++ tape_en_cours/mysite/polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Client(models.Model):
     prenom = models.CharField(max_length=50)
     date_de_naissance = models.DateField()
+    toto = 3
 
     def __str__(self):
         return f"{self.prenom} - {self.date_de_naissance} ans"
