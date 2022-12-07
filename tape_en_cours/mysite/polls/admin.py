9bfc32d6429d0042823a0f702a4229e8ac66a54f
Matthieu Falce <matthieu@falce.net>
2022-12-07 15:59:21 +0100

    gitautosync update 155

--- tape_en_cours/mysite/polls/admin.py
+++ tape_en_cours/mysite/polls/admin.py
@@ -3,6 +3,28 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+from django.db.models.functions import ExtractYear
+
+
+class SearchByYear(admin.SimpleListFilter):
+    title = _("title")
+    parameter_name = "year"
+
+    def lookups(self, request, model_admin):
+        year_list = (
+            models.Event.objects.annotate(y=ExtractYear("date"))
+            .order_by("y")
+            .values_list("y", flat=True)
+            .distinct()
+        )
+        return [(str(y), _(str(y))) for y in year_list]
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            return queryset.filter(date__year=self.value())
+        return queryset
+
+
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("question_text", "pub_date")
     list_filter = ("pub_date",)
