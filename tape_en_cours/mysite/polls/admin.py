7f97cb862c6fe94b65d16fdc30148d4e30d15de3
Matthieu Falce <matthieu@falce.net>
2022-12-07 16:38:18 +0100

    gitautosync update 176

--- tape_en_cours/mysite/polls/admin.py
+++ tape_en_cours/mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-from django.db.models.functions import ExtractYear
+from django.db.models.functions import ExtractYear, Lower
 
 
 class SearchByYear(admin.SimpleListFilter):
