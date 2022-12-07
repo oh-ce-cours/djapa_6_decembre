45e0761b27edf963eebce44f8a8130726816d251
Matthieu Falce <matthieu@falce.net>
2022-12-07 14:55:45 +0100

    gitautosync update 117

--- tape_en_cours/mysite/polls/models.py
+++ tape_en_cours/mysite/polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-    def was_published_recently(self):
+    def was_published_recently(self: "Question") -> bool:
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
