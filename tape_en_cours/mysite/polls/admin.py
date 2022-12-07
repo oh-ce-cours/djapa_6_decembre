a13d624acf7603f278d2e65f4fe6faa1d5313652
Matthieu Falce <matthieu@falce.net>
2022-12-07 15:50:31 +0100

    gitautosync update 149

--- tape_en_cours/mysite/polls/admin.py
+++ tape_en_cours/mysite/polls/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name")
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
