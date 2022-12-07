b3ce1ebb05a088c2e2998e2508d707cf01aae076
Matthieu Falce <matthieu@falce.net>
2022-12-07 14:43:06 +0100

    gitautosync update 112

--- tape_en_cours/mysite/polls/models.py
+++ tape_en_cours/mysite/polls/models.py
@@ -5,7 +5,9 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    # ...
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -16,7 +18,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)  # ...
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
