329a90d53ef6037019fb229112f8ee03ae2f0e82
Matthieu Falce <matthieu@falce.net>
2022-12-07 14:42:42 +0100

    gitautosync update 110

--- tape_en_cours/mysite/polls/models.py
+++ tape_en_cours/mysite/polls/models.py
@@ -8,3 +8,16 @@ class Question(models.Model):
     # ...
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def __str__(self):
+        return self.question_text
+
+
+from django.db import models
+
+class Question(models.Model):
+    # ...
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
