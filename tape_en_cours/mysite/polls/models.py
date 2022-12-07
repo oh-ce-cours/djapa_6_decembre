c5dd53f249d04f0ab6675c5ffa3b3db07f23b8f1
Matthieu Falce <matthieu@falce.net>
2022-12-07 16:59:04 +0100

    gitautosync update 191

--- tape_en_cours/mysite/polls/models.py
+++ tape_en_cours/mysite/polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    owner = models.ForeignKey("User")
 
     def was_published_recently(self: "Question") -> bool:
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
