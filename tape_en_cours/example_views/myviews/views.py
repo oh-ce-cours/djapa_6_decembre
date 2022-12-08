c9f1cfc0148e1fbf0e9661e6335ac0abc44d8e4b
Matthieu Falce <matthieu@falce.net>
2022-12-08 15:38:14 +0100

    gitautosync update 266

--- tape_en_cours/example_views/myviews/views.py
+++ tape_en_cours/example_views/myviews/views.py
@@ -13,7 +13,7 @@ def fbv_python(request):
 def fbv_template(request):
     template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list': latest_question_list,
+        'prenoms': prenoms,
     }
     return HttpResponse(template.render(context, request))
 
