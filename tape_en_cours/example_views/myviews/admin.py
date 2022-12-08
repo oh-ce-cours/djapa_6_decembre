53c9ccee746363d45407bd297f6507119f6a1b23
Matthieu Falce <matthieu@falce.net>
2022-12-08 17:36:16 +0100

    gitautosync update 341

--- tape_en_cours/example_views/myviews/admin.py
+++ tape_en_cours/example_views/myviews/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Exemple
 
+
+@admin.action(description="Send email")
+def make_published(modeladmin, request, queryset):
+    queryset.update(status="p")
+
+
 for obj in queryset:
     do_something_with(obj)
 
