c1336438abebff7ad986b9d0541d85496a15a4e0
Matthieu Falce <matthieu@falce.net>
2022-12-08 16:18:05 +0100

    gitautosync update 313

--- tape_en_cours/example_views/myviews/views.py
+++ tape_en_cours/example_views/myviews/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
 # Create your views here.
 from django.views.generic import TemplateView
 from typing import Any, Dict
 
 
 prenoms = ["Matthieu", "Pierre", "Ernest"]
+
 
 def fbv_python(request):
     inside_ul = "".join([f"<li>{item}</li>" for item in prenoms])
@@ -15,27 +17,24 @@ def fbv_python(request):
     # ul="<script> alert('bouh') </script>"
     return HttpResponse(ul)
 
+
 def fbv_template(request):
-    template = loader.get_template('myviews/index.html')
-    context = {
-        'prenoms': prenoms,
-        "custom": "Dans fbv_template"
-    }
+    template = loader.get_template("myviews/index.html")
+    context = {"prenoms": prenoms, "custom": "Dans fbv_template"}
     return HttpResponse(template.render(context, request))
 
+
 def fbv_shortcut(request):
-    context = {
-        'prenoms': prenoms,
-        "custom": "Dans fbv_shortcut"
-    }
-    return render(request, "myviews/index.html", context) 
+    context = {"prenoms": prenoms, "custom": "Dans fbv_shortcut"}
+    return render(request, "myviews/index.html", context)
+
 
 class CBV(TemplateView):
-    template_name="myviews/index.html"
+    template_name = "myviews/index.html"
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return {
-            **super().get_context_data(**kwargs), 
-            'prenoms': prenoms,
-            "custom": "Dans CBV"
+            **super().get_context_data(**kwargs),
+            "prenoms": prenoms,
+            "custom": "Dans CBV",
         }
