bd1e461fab3728cf26219dfdf86ae103c9c92a6f
Matthieu Falce <matthieu@falce.net>
2022-12-08 15:28:00 +0100

    gitautosync update 246

--- tape_en_cours/example_views/example_views/urls.py
+++ tape_en_cours/example_views/example_views/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("demo/", include("myviews.urls"))
+    path("demo/", include("myviews.urls")),
     path("admin/", admin.site.urls),
 ]
