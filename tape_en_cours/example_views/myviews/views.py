025f78d318c920441c2885cdfb702bef01ab9995
Matthieu Falce <matthieu@falce.net>
2022-12-08 15:37:51 +0100

    gitautosync update 264

--- tape_en_cours/example_views/myviews/views.py
+++ tape_en_cours/example_views/myviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 # Create your views here.
 
 prenoms = ["Matthieu", "Pierre", "Ernest"]
