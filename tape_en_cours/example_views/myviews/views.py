0bb0c430d061cd6908d4be2493d0a9ca88aa957f
Matthieu Falce <matthieu@falce.net>
2022-12-08 15:34:46 +0100

    gitautosync update 255

--- tape_en_cours/example_views/myviews/views.py
+++ tape_en_cours/example_views/myviews/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def fbv_python(request):
+    pass 
+
+def fbv_template(request):
+    pass 
+def fbv_shortcut(request):
+    pass 
+cbv
