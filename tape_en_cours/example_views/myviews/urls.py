7186acc286eca9f09fab4665eabda5084d647032
Matthieu Falce <matthieu@falce.net>
2022-12-08 15:29:35 +0100

    gitautosync update 249

---
+++ tape_en_cours/example_views/myviews/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("articles/2003/", views.special_case_2003),
+    path("articles/<int:year>/", views.year_archive),
+]
