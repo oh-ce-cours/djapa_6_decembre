8a9ad51c9147bf729bfe867d0bca6507308e8ad7
Matthieu Falce <matthieu@falce.net>
2022-12-08 17:36:39 +0100

    gitautosync update 343

--- tape_en_cours/example_views/myviews/admin.py
+++ tape_en_cours/example_views/myviews/admin.py
@@ -4,11 +4,9 @@ from .models import Exemple
 
 @admin.action(description="Send email")
 def send_email(modeladmin, request, queryset):
-    queryset.update(status="p")
+    for obj in queryset:
+        do_something_with(obj)
 
-
-for obj in queryset:
-    do_something_with(obj)
 
 # Register your models here.
 admin.site.register(Exemple)
