977265ba7acf565db38773686c86f7eb5fab955b
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:12:24 +0100

    gitautosync update 360

--- tape_en_cours/anniversaire/rappel_date/admin.py
+++ tape_en_cours/anniversaire/rappel_date/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Exemple
+
+
+@admin.action(description="Send email")
+def send_email(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.send_email()
+
+
+class ExempleAdmin(admin.ModelAdmin):
+    actions = [send_email]
+
 
 # Register your models here.
+admin.site.register(Exemple, ExempleAdmin)
