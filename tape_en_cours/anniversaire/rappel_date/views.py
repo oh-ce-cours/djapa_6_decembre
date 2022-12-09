0f22354603c5171a6b8a776fcd567b306b7fdb5d
Matthieu Falce <matthieu@falce.net>
2022-12-09 11:55:53 +0100

    gitautosync update 552

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -20,4 +20,7 @@ def notify(request, anniversaire_pk):
 def details(request, anniversaire_pk, nom):
     anniversaire = get_object_or_404(Anniversaire, pk=anniversaire_pk)
     context = {"item": anniversaire}
+    if nom != anniversaire.nom:
+    return redirect("anniversaire:details", nom=anniversaire.nom, anniversaire_pk=anniversaire.pk)
+
     return render(request, "rappel_date/details.html", context)
