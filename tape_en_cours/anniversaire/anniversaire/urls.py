f87719e2bf92a0dadb848c90a46facd41a7f70d5
Matthieu Falce <matthieu@falce.net>
2022-12-09 14:25:44 +0100

    gitautosync update 655

--- tape_en_cours/anniversaire/anniversaire/urls.py
+++ tape_en_cours/anniversaire/anniversaire/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
@@ -21,4 +22,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("rappel_date.urls")),
     path("accounts/", include("django.contrib.auth.urls")),  # new
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
