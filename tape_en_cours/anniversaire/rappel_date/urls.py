423fc7d3e39bffa194691b85a09ea225c3d08978
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:12:01 +0100

    gitautosync update 358

--- tape_en_cours/anniversaire/rappel_date/urls.py
+++ tape_en_cours/anniversaire/rappel_date/urls.py
@@ -4,9 +4,4 @@ from . import views
 
 urlpatterns = [
     path("1", views.fbv_python),
-    path("2", views.fbv_template),
-    path("3", views.fbv_shortcut),
-    path("4", views.CBV_verbeux.as_view()),
-    path("5", views.CBV_opti.as_view()),
-    path("6", views.CBV_complexe.as_view()),
 ]
