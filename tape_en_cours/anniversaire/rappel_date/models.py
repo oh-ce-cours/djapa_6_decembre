d239b1f159d06b7c8b821b2b445eec0ebdec7242
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:12:13 +0100

    gitautosync update 359

--- tape_en_cours/anniversaire/rappel_date/models.py
+++ tape_en_cours/anniversaire/rappel_date/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Exemple(models.Model):
+    email = models.EmailField()
+
+    def send_email(self):
+        print("salut", self.email)
