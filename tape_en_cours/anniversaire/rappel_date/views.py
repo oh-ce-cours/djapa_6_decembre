90b92a33287ec66a7b320b8ccb1c676393c9f8f5
Matthieu Falce <matthieu@falce.net>
2022-12-09 15:56:50 +0100

    gitautosync update 720

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -61,7 +61,9 @@ def bulk(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES["file"])
+            import ipdb
+
+            ipdb.set_trace()
             return HttpResponseRedirect("/success/url/")
     else:
         form = UploadFileForm()
