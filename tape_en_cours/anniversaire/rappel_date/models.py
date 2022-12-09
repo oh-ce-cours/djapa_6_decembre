0ac6f591116396e2a3c3de500c8565dd8ed695b6
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:45:56 +0100

    gitautosync update 402

--- tape_en_cours/anniversaire/rappel_date/models.py
+++ tape_en_cours/anniversaire/rappel_date/models.py
@@ -12,7 +12,7 @@ class Notification(models.Model):
     message = models.TextField()
 
     class Meta:
-        plural_verbose_name = "Real estate properties"
+        verbose_name_plural = "Real estate properties"
 
     def __str__(self):
         return f"{self.anniversaire.nom}, {self.anniversaire.prenom}, {self.datetime}"
