44cce2237e7c2383ba78f41b7eda5793c0e7e69f
Matthieu Falce <matthieu@falce.net>
2022-12-09 12:06:13 +0100

    gitautosync update 570

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     context = {
         "anniversaires": Anniversaire.objects.filter(),
