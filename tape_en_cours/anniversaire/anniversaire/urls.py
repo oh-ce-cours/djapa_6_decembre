7129fa8dfaf3382118f4d50534f768115549534e
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:11:15 +0100

    gitautosync update 354

--- tape_en_cours/anniversaire/anniversaire/urls.py
+++ tape_en_cours/anniversaire/anniversaire/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("anniv/", include("rappel_date.urls")),
+    path("", include("rappel_date.urls")),
 ]
