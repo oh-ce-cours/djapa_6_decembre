bd4170761054797af2c3d9941fa1ac735308135b
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:51:14 +0100

    gitautosync update 407

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Anniversaire
 
 # Create your views here.
 def index(request):
+    context = {}
     render(request, "rappel_date/index.html", context)
