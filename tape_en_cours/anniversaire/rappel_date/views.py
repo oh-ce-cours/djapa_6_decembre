6146c2096b70674e6af10e99da9790229db01657
Matthieu Falce <matthieu@falce.net>
2022-12-09 12:38:51 +0100

    gitautosync update 609

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -15,9 +15,6 @@ def index(request):
     # else:
     #     anniversaires = Anniversaire.objects.filter(owner=request.user)
     ####
-    import ipdb
-
-    ipdb.set_trace()
     anniversaires = Anniversaire.get_allowed_for_user(request.user)
     context = {
         "request": request,
