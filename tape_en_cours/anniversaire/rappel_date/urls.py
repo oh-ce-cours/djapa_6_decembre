115cf7e10159bead2ff8a3008ce2f3b851914404
Matthieu Falce <matthieu@falce.net>
2022-12-09 11:00:36 +0100

    gitautosync update 484

--- tape_en_cours/anniversaire/rappel_date/urls.py
+++ tape_en_cours/anniversaire/rappel_date/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("notify", views.notify, name="notify"),
 ]
