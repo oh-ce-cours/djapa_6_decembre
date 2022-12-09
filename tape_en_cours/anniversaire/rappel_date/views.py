3add845c737ed8eb8a02009cda9f39334cf2dd46
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:55:49 +0100

    gitautosync update 416

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -6,4 +6,4 @@ def index(request):
     context = {
         "anniversaires": Anniversaire.objects.all(),
     }
-    render(request, "rappel_date/index.html", context)
+    return render(request, "rappel_date/index.html", context)
