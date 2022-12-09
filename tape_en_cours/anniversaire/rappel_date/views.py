2a35d0c872a4a84f416082fee254520dde88e7e3
Matthieu Falce <matthieu@falce.net>
2022-12-09 09:52:07 +0100

    gitautosync update 409

--- tape_en_cours/anniversaire/rappel_date/views.py
+++ tape_en_cours/anniversaire/rappel_date/views.py
@@ -3,5 +3,7 @@ from .models import Anniversaire
 
 # Create your views here.
 def index(request):
-    context = {"anniversaires": Anniversaire.objects.all()}
+    context = {
+        "anniversaires": Anniversaire.objects.all(),
+    }
     render(request, "rappel_date/index.html", context)
