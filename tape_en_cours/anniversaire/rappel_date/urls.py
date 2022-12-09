37f93cc15d66e6f9a6c79f3b7ba300d6533b0825
Matthieu Falce <matthieu@falce.net>
2022-12-09 11:09:35 +0100

    gitautosync update 507

--- tape_en_cours/anniversaire/rappel_date/urls.py
+++ tape_en_cours/anniversaire/rappel_date/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "anniversaire"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("notify/<int:anniversaire_pk>", views.notify, name="notify"),
