78954e5ee7718fb7c3cf689ea568c989cbcb77e4
Matthieu Falce <matthieu@falce.net>
2022-12-09 14:27:21 +0100

    gitautosync update 659

--- tape_en_cours/anniversaire/anniversaire/urls.py
+++ tape_en_cours/anniversaire/anniversaire/urls.py
@@ -23,5 +23,3 @@ urlpatterns = [
     path("", include("rappel_date.urls")),
     path("accounts/", include("django.contrib.auth.urls")),  # new
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-print(f"{settings.MEDIA_URL}, {settings.MEDIA_ROOT}")
